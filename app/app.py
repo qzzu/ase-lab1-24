b1b3dea7bab12fd0eed69fdda26977c4a450e794
qzzu <laltylmirch@gmail.com>
2025-11-17 13:34:53 +0100

    bonus_pt1

--- app/app.py
+++ app/app.py
@@ -57,6 +57,46 @@ def mod():
         return make_response('Invalid input\n', 200)
 
 #Endpoint /random which takes a and b as query parameters and returns a random number between a and b included. Returns HTTP 400 BAD REQUEST if a is greater than b.
+@app.route('/random')
+def random():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        if a > b:
+            return make_response('ERROR: a > b', 400)
+        else:
+            return make_response(jsonify(s = random.randint(a,b)), 200)
+    else:
+        return make_response('Invalid input\n', 200)
+
+# upper 
+@app.route('/upper')
+def upper():
+    a = request.args.get('a', type=str)
+    if a:
+        return make_response(jsonify(x=a.upper()), 200)
+    else:
+        return make_response('Invalid input\n', 200)
+
+# lower
+@app.route('/lower')
+def lower():
+    a = request.args.get('a', type=str)
+    if a:
+        return make_response(jsonify(x=a.lower()), 200)
+    else:
+        return make_response('Invalid input\n', 200)
+
+#concat
+@app.route('/concat')
+def concat():
+    a = request.args.get('a', type=str)
+    b = request.args.get('b', type=str)
+    if a and b:
+        return make_response(jsonify(x = a + b), 200)
+    else:
+        return make_response('Invalid input\n', 200)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
